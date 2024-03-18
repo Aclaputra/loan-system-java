2af66248b7fbf6ccf31bffc14e2dc5f891e33d48
Muhammad Acla <aclalead@gmail.com>
2024-03-18 09:00:51 +0700

    feat: add constant path

---
+++ src/main/java/com/enigmacamp/constant/ApiPathConstant.java
@@ -0,0 +1,19 @@
+package com.enigmacamp.constant;
+
+public class ApiPathConstant {
+    public static final String AUTH = "/auth";
+    public static final String SIGN_UP = "/signup";
+    public static final String SIGN_IN = "/signin";
+    public static final String PARAM_ID = "/{id}";
+    public static final String USER = "/users";
+    public static final String CUSTOMER = "/customers";
+    public static final String PARAM_CUSTOMER_ID = "/{customerId}";
+    public static final String INSTALLMENT_TYPE = "/instalment-types";
+    public static final String LOAN_TYPE = "/loan-types";
+    public static final String TRANSACTION = "/transactions";
+    public static final String PARAM_ADMIN_ID = "/{adminId}";
+    public static final String APPROVE = "/approve";
+    public static final String PARAM_TRX_ID = "/{trxId}";
+    public static final String PAY = "/pay";
+
+}
