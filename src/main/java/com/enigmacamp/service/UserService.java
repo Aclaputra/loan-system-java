e52df097d74434d55a75c7be2572df397a4fac2a
Muhammad Acla <aclalead@gmail.com>
2024-03-19 07:27:36 +0700

    feat: add token filter

--- src/main/java/com/enigmacamp/service/UserService.java
+++ src/main/java/com/enigmacamp/service/UserService.java
@@ -2,8 +2,10 @@ package com.enigmacamp.service;
 
 import com.enigmacamp.model.dto.request.user.UserRequest;
 import com.enigmacamp.model.dto.response.user.UserResponse;
+import com.enigmacamp.model.entity.AppUser;
 import org.springframework.stereotype.Service;
 
 public interface UserService {
     UserResponse getById(String id);
+    AppUser loadUserByUserId(String userId);
 }
